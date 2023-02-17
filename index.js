5c369bd277a02fd040331d149430b48bb3be856b
vctor03 <victordicipulo@gmail.com>
2023-02-17 18:31:00 -0300

    interface-de-cores

--- index.js
+++ index.js
@@ -7,52 +7,64 @@ const button6 = document.getElementById("button-6");
 const button7 = document.getElementById("button-7");
 const button8 = document.getElementById("button-8");
 const caixaDeCores = document.getElementById("caixa-de-cores");
+const themeWhite = document.getElementById("themewhite");
+const themeBtn = document.getElementById("themebtn");
 
 function mudarCor(color) {
   caixaDeCores.style.background = color;
 }
-alert("Seja bem-vindo ( a )");
+// alert("Seja bem-vindo ( a )");
 button1.addEventListener("mouseover", () => {
   mudarCor(
-    "linear-gradient(0deg, rgba(65,41,41,1) 32%, rgba(75,47,47,1) 39%, rgba(184,66,66,1) 72%)"
+    "linear-gradient(0deg, rgba(190,67,67,1) 32%, rgba(190,58,58,1) 39%, rgba(255,98,98,1) 72%)"
   );
 });
 button2.addEventListener("mouseover", () => {
   mudarCor(
-    "linear-gradient(0deg, rgba(42,41,65,1) 32%, rgba(49,47,75,1) 39%, rgba(66,70,184,1) 72%)"
+    "linear-gradient(0deg, rgba(67,72,190,1) 32%, rgba(58,68,190,1) 39%, rgba(98,131,255,1) 72%)"
   );
 });
 button3.addEventListener("mouseover", () => {
   mudarCor(
-    "linear-gradient(0deg, rgba(41,65,44,1) 32%, rgba(47,75,47,1) 39%, rgba(82,184,66,1) 72%)"
+    "linear-gradient(0deg, rgba(72,190,67,1) 32%, rgba(58,190,73,1) 39%, rgba(98,255,144,1) 72%)"
   );
 });
 button4.addEventListener("mouseover", () => {
   mudarCor(
-    "linear-gradient(0deg, rgba(74,43,72,1) 32%, rgba(84,51,85,1) 39%, rgba(184,66,181,1) 72%)"
+    "linear-gradient(0deg, rgba(190,67,184,1) 32%, rgba(190,58,188,1) 39%, rgba(255,98,255,1) 72%)"
   );
 });
 button5.addEventListener("mouseover", () => {
   mudarCor(
-    "linear-gradient(0deg, rgba(73,74,43,1) 32%, rgba(84,85,51,1) 39%, rgba(196,193,65,1) 72%)"
+    "linear-gradient(0deg, rgba(190,188,67,1) 32%, rgba(190,186,58,1) 39%, rgba(252,255,98,1) 72%)"
   );
 });
 button6.addEventListener("mouseover", () => {
   mudarCor(
-    " linear-gradient(0deg, rgba(41,65,65,1) 32%, rgba(47,74,75,1) 39%, rgba(66,184,179,1) 72%)"
+    " linear-gradient(0deg, rgba(67,190,190,1) 32%, rgba(58,188,190,1) 39%, rgba(98,252,255,1) 72%)"
   );
 });
 button7.addEventListener("mouseover", () => {
   mudarCor(
-    "linear-gradient(0deg, rgba(74,66,43,1) 32%, rgba(85,75,51,1) 39%, rgba(196,142,65,1) 72%)"
+    "linear-gradient(0deg, rgba(190,139,67,1) 32%, rgba(190,133,58,1) 39%, rgba(255,187,98,1) 72%)"
   );
 });
 button8.addEventListener("mouseover", () => {
   mudarCor(
-    "linear-gradient(0deg, rgba(74,43,70,1) 32%, rgba(85,51,79,1) 39%, rgba(142,65,196,1) 72%)"
+    "linear-gradient(0deg, rgba(165,67,190,1) 32%, rgba(164,58,190,1) 39%, rgba(197,98,255,1) 72%)"
   );
+});
+caixaDeCores.addEventListener("click", () => {
+  caixaDeCores.style.background = "";
 });
 
 window.sr = ScrollReveal({ reset: true });
+sr.reveal(".caixa", { duration: 5000 });
 
-sr.reveal(".caixa", { duration: 6500 });
+themeBtn.addEventListener("click", () => {
+  if (document.documentElement.classList.contains("themewhite")) {
+    document.documentElement.classList.remove("themewhite");
+  } else {
+    document.documentElement.classList.add("themewhite");
+  }
+});
